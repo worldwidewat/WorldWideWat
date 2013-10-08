8e2d642e813dde8c588642fb027518ddec462134
mharen <mharen@gmail.com>
2013-10-08 07:49:56 -0400

    data-backed model

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
@@ -13,7 +14,49 @@ namespace Web.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var model = new IndexModel();
+
+            model.Tiles.Add(new Tile
+                                {
+                                    Title = "upboat.me",
+                                    Description = @"Build a meme from anywhere by linking 
+                                                    to upboat.me. Put your meme info right 
+                                                    in the url.",
+                                    Url = "http://upboat.me",
+                                    ImageUrl = "http://placekitten.com/300/100"
+                                });
+
+            model.Tiles.Add(new Tile
+                                {
+                                    Title = "iffylink.com",
+                                    Description =
+                                        @"A volatile URL-shortener. Nothing lasts forever, 
+                                          why should short links?",
+                                    Url = "http://iffylink.com",
+                                    ImageUrl = "http://placekitten.com/301/100"
+                                });
+
+            model.Tiles.Add(new Tile
+                                {
+                                    Title = "tickstodatetime.com",
+                                    Description =
+                                        @"Convert .NET <code>Ticks</code> to a human-readable 
+                                          <code>DateTime</code>.",
+                                    Url = "http://tickstodatetime.com",
+                                    ImageUrl = "http://placekitten.com/302/100"
+                                });
+
+            model.Tiles.Add(new Tile
+                                {
+                                    Title = "datetimetostring.com",
+                                    Description =
+                                        @"Quick and direct access to all those obscure format
+                                          strings for .NET's <code>DateTime</code>.",
+                                    Url = "http://datetimetostring.com",
+                                    ImageUrl = "http://placekitten.com/303/100"
+                                });
+
+            return View(model);
         }
 
     }
---
+++ Web/Models/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class Tile
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set;}
+        public string ImageUrl { get; set; }
+    }
+}
